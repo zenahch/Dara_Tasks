67f45921e4e7d5091d43052d7aa6ac5124d2e092
Zenah Chehab <z_ch_83@hotmail.com>
2023-06-16 12:17:21 -0400

    Created IsOddAndIsEven.java

---
+++ src/IsOddAndIsEven.java
@@ -0,0 +1,25 @@
+public class IsOddAndIsEven {
+    public static void main(String[] args) {
+
+        System.out.println(isOdd(5));
+        System.out.println(isEven(6));
+
+    }
+    public static boolean isOdd(int number){
+        if (number % 2 != 0){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+    public static boolean isEven(int number){
+        if ( number % 2 == 0){
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+
+}
