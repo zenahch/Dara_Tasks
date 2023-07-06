ea0678df51ede706c0f1c7294ba9aa68b7780f40
Zenah Chehab <z_ch_83@hotmail.com>
2023-07-06 14:19:23 -0400

    Added ArrayAscending_DescendingOrder.java

---
+++ src/ArrayAscending_DescendingOrder.java
@@ -0,0 +1,19 @@
+public class ArrayAscending_DescendingOrder {
+    public static void main(String[] args) {
+        int[] array = new int[]{45,77,32,65,87};
+        int x;
+        for(int i=0; i < array.length; i++){
+            for(int j=i; j< array.length; j++ ){
+                if(array[i] > array[j]) {
+                    x = array[i];
+                    array[i] = array[j];
+                    array[j] = x;
+                }
+            }
+        }
+
+        for(int a : array){
+            System.out.println(a);
+        }
+    }
+}
